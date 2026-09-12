e6c89d2b328b94975227303d6bab08edc6665a97
Anas0473 <anasbutthussain@gmail.com>
2026-09-13 00:55:24 +0200

    Update admin configuration and project files

--- src/content.config.ts
+++ src/content.config.ts
@@ -3,18 +3,18 @@ import { glob } from 'astro/loaders';
 import { z } from 'astro/zod';
 
 const blog = defineCollection({
-	// Load Markdown and MDX files in the `src/content/blog/` directory.
-	loader: glob({ base: './src/content/blog', pattern: '**/*.{md,mdx}' }),
-	// Type-check frontmatter using a schema
-	schema: ({ image }) =>
-		z.object({
-			title: z.string(),
-			description: z.string(),
-			// Transform string to Date object
-			pubDate: z.coerce.date(),
-			updatedDate: z.coerce.date().optional(),
-			heroImage: z.optional(image()),
-		}),
+  loader: glob({ base: './src/content/blog', pattern: '**/*.{md,mdx}' }),
+  schema: () =>
+    z.object({
+      title: z.string(),
+      description: z.string().optional(),
+      pubDate: z.union([z.string(), z.coerce.date()]).optional(),
+      updatedDate: z.coerce.date().optional(),
+      heroImage: z.string().optional(),
+      author: z.string().optional(),
+      isbn: z.string().optional(),
+      status: z.enum(['Reading', 'Read', 'Plan to Read']).optional(),
+    }),
 });
 
 export const collections = { blog };
---
+++ src/content/blog/daw.md
@@ -0,0 +1,8 @@
+---
+title: daw
+author: "312"
+isbn: "123"
+pubDate: "2114"
+status: Reading
+heroImage: https://i.imgur.com/KFafrPG.png
+---
